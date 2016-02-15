b41270ce9d97ced0abed34c36cd0977893721408
umayr <s51570@tm.com.my>
2016-02-15 15:05:36 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 # This project requires that the dataset downloaded from the UCI repositiory as per instruction page (readme.md)
 
-nzip("exdata_data_household_power_consumption.zip", overwrite = T)
+unzip("exdata_data_household_power_consumption.zip", overwrite = T)
 datasource <- "household_power_consumption.txt"
 data <- read.table(datasource, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
